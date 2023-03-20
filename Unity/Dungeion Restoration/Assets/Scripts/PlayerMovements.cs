0dc08da64ba00bbda162d3b11d443a51b4b97ead
Xeleolon <nathan.cattell@outlook.com>
2023-03-20 22:23:16 +0800

    Basic Camara Rotation
    Set up Basic Camera Rotation For Player

--- Unity/Dungeion Restoration/Assets/Scripts/PlayerMovements.cs
+++ Unity/Dungeion Restoration/Assets/Scripts/PlayerMovements.cs
@@ -8,14 +8,49 @@ public class PlayerMovements : MonoBehaviour
     public float sprintSpeed = 6;
     public float jump = 1;
     public bool OnGround;
+    //Movement
     private Vector3 velocity;
     private Rigidbody rb;
-
-
+    //Camera Control
+    [Tooltip("The rotation acceleration, in degrees / second")]
+    [SerializeField] private Vector2 cameraAcceleration;
+    [Tooltip("A mutipler to the input. Describes the maximum speed in degrees / second.")]
+    [SerializeField] private Vector2 cameraSensitivity;
+    [Tooltip("The Maximum angle from the horizon the player can rotote, in degrees")]
+    [SerializeField] private float cameraMaxVerticalAngleFromHorizon;
+    [Tooltip("The period to wait until resetting the input value. Set this as low as possible without encountering stuttering from camera")]
+    [SerializeField] private float cameraInputLagPeriod;
+    private GameObject mainCamera;
+    private Vector2 cameraRotation;
+    private Vector2 cameraVelocity;
+    private Vector2 cameraLastInputEvent;
+    private float cameraInputLagTimer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        mainCamera = GameObject.FindWithTag("MainCamera");
+    }
+    void OnEnable()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+        cameraVelocity = Vector2.zero;
+        cameraInputLagTimer = 0;
+        cameraLastInputEvent = Vector2.zero;
+
+        Vector3 euler = transform.localEulerAngles;
+        euler.x = mainCamera.transform.localEulerAngles.x;
+        if(euler.x >= 180)
+        {
+            euler.x -= 360;
+        }
+        euler.x = ClampCameraVerticalAngle(euler.x);
+
+        transform.localEulerAngles = new Vector3(0, euler.y, euler.z);
+        mainCamera.transform.localEulerAngles = new Vector3(euler.x,0 , 0);
+
+        cameraRotation = new Vector2(euler.y, euler.x);
+        
     }
 
     void Update()
@@ -24,6 +59,25 @@ public class PlayerMovements : MonoBehaviour
         {
         MovementInputs();
         }
+
+
+
+        // camera Velocity is currenty mouse Input scaled by desired sensitivity
+        // this is the maximum velocity
+        Vector2 cameraSpeed = GetMouseInput() * cameraSensitivity;
+
+        // Calculate new rotation and store it for future changes
+        cameraVelocity = new Vector2(
+            Mathf.MoveTowards(cameraVelocity.x, cameraSpeed.x, cameraAcceleration.x * Time.deltaTime),
+            Mathf.MoveTowards(cameraVelocity.y, cameraSpeed.y, cameraAcceleration.y * Time.deltaTime));
+        
+        cameraRotation += cameraVelocity * Time.deltaTime;
+
+        cameraRotation.y = ClampCameraVerticalAngle(cameraRotation.y);
+
+        // convert the camera rotation to euler angles 
+        transform.localEulerAngles = new Vector3(0, cameraRotation.x, 0);
+        mainCamera.transform.localEulerAngles = new Vector3(cameraRotation.y, 0 ,0);
     }
     void FixedUpdate()
     {
@@ -31,6 +85,26 @@ public class PlayerMovements : MonoBehaviour
         //{
         rb.MovePosition(rb.position + transform.TransformDirection(velocity) * Time.fixedDeltaTime);
         //}
+    }
+    private float ClampCameraVerticalAngle(float angle)
+    {
+        return Mathf.Clamp(angle, -cameraMaxVerticalAngleFromHorizon, cameraMaxVerticalAngleFromHorizon);
+    }
+    private Vector2 GetMouseInput()
+    {
+        cameraInputLagTimer += Time.deltaTime;
+
+
+        Vector2 mouseInput = new Vector2(Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"));
+
+        if ((Mathf.Approximately(0, mouseInput.x) && Mathf.Approximately(0, mouseInput.y)) == false || cameraInputLagTimer >= cameraInputLagPeriod)
+        {
+            cameraLastInputEvent = mouseInput;
+            cameraInputLagTimer = 0;
+        }
+
+        
+        return cameraLastInputEvent;
     }
     void MovementInputs()
     {
