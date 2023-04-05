ffcfa9e3457f2ece6ac96124bb4ce5951e8e0943
Xeleolon <nathan.cattell@outlook.com>
2023-04-06 00:53:43 +0800

    mainV024
    Added Slider To Replinsih Interactables

--- Unity/Dungeion Restoration/Assets/Scripts/Interactables/ReplenishInteract.cs
+++ Unity/Dungeion Restoration/Assets/Scripts/Interactables/ReplenishInteract.cs
@@ -1,13 +1,54 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReplenishInteract : Interactable
 {
+    [Header("Replinish Variables")]
+    private GameObject ReplinishCanvas;
+    public GameObject sliderPrefab;
+    public float maxValue;
+    public float fillSpeed = 0.1f;
+    private float fillValue = 0;
+    Slider bar;
+    public Gradient gradient;
+    Image fill;
+    Transform playerHead;
+    public float speed = 1.0f;
+    void Start()
+    {
+        playerHead = GameObject.FindWithTag("MainCamera").transform;
+
+    }
+    void Update()
+    {
+        if (ReplinishCanvas != null)
+        {
+            //Transform Ui To Face Player
+            Vector3 targetRotation = playerHead.position - ReplinishCanvas.transform.position;
+            float step = speed * Time.deltaTime;
+            Vector3 newRotation = Vector3.RotateTowards(ReplinishCanvas.transform.forward, targetRotation, step, 0.0f);
+            ReplinishCanvas.transform.rotation = Quaternion.LookRotation(newRotation);
+
+            bar.value = Mathf.Lerp(bar.value, fillValue, Time.deltaTime);
+            fill.color = gradient.Evaluate(bar.normalizedValue);
+
+            if (fillValue >= (maxValue * 2))
+            {
+                Destroy(ReplinishCanvas);
+            }
+            else
+            {
+                fillValue += fillSpeed * Time.deltaTime;
+            }
+        }
+    }
     public override void Interact()
     {
         base.Interact();
         if (interactionState == 3)
         {
             PlayAnimator();
+            ActivateSlider();
             string message = new string(gameObject.name + " Replenish it supples");
             Debug.Log(message);
             PlayerChat.instance.NewMessage(message);
@@ -23,6 +64,15 @@ public class ReplenishInteract : Interactable
     public override void PlayAnimator()
     {
         base.PlayAnimator();
+    }
+    private void ActivateSlider()
+    {
+        ReplinishCanvas = Instantiate(sliderPrefab, transform.position, Quaternion.identity);
+        bar = ReplinishCanvas.transform.Find("Slider").GetComponent<Slider>();
+        fill = bar.transform.Find("Fill Area").transform.Find("Fill").GetComponent<Image>();
+
+        bar.maxValue = maxValue;
+        bar.value = 0;
     }
 
 }
