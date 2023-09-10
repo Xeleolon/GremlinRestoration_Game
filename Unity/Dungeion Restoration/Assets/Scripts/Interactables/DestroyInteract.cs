7ab558715976421eaf57c658017f0bafed41c7de
Xeleolon <nathan.cattell@outlook.com>
2023-09-11 02:16:03 +0800

    mainV.2.41
    level 4 mostly done

--- Unity/Dungeion Restoration/Assets/Scripts/Interactables/DestroyInteract.cs
+++ Unity/Dungeion Restoration/Assets/Scripts/Interactables/DestroyInteract.cs
@@ -21,6 +21,7 @@ public class DestroyInteract : Interactable
             Completed();
             FinishTask();
             SpawnEffect(false);
+            Activate(false);
             if (pressurePlate != null)
             {
                 pressurePlate.ForceExit(gameObject);
